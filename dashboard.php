0a037b9f6fe71e2fa504d2aa9950ed4005daa5d4
Andrea-Morabito <andreamorabito05@gmail.com>
2024-01-24 17:55:12 +0100

    Fix Dashboard, added divSelector.js, Fix Query User

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
     $connessione = Connection::new();
     $userMail = $_POST['UserMail'];
     // Get UserMail from table Utenti
-    $query = "SELECT email FROM utenti WHERE email = $userMail";
+    $query = "SELECT email FROM utenti WHERE email = '".$userMail."'";
     $result = $connessione->query($query);
 
     if ($result->num_rows > 0) {
@@ -24,19 +24,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="styles/dashboard.css" rel="stylesheet" />
+    <script src="script/divSelector.js"></script>
 </head>
 <body>
-    <div class="item1">
+    <div class="itemsMargin" id="dashboardSidebar">
+        <button onclick="showDiv(1)">Button 1</button>
+        <button onclick="showDiv(2)">Button 2</button>
+        <button onclick="showDiv(3)">Button 3</button>
+    </div>
+
+    <div class="itemsMargin" id="dashboardHeader">
 
     </div>
 
-    <div class="item2">
+    <main class="itemsMargin" id="dashboardMainContent">  
+        <div id="div1" class="content-div gridTemplate">
+            <div class="item">
 
-    </div>
+            </div>
+            <div class="item">
 
-    <div class="item3">
+            </div>
+            <div class="item">
 
-    </div>
+            </div>
+            <div class="item">
+
+            </div>
+        </div>
+
+        <div id="div2" class="content-div gridTemplate">
+            <div class="item">
+            </div>
+
+            <div class="item">
+            </div>
+
+            <div class="item">
+            </div>
+
+            <div class="item">
+            </div>
+        </div>
+
+        <div id="div3" class="content-div gridTemplate">
+            <div class="item">
+            </div>
+            <div class="item">
+
+            </div>
+            <div class="item">
+
+            </div>
+            <div class="item">
+
+            </div>
+        </div>
+    </main>
 </body>
 </html>
 
